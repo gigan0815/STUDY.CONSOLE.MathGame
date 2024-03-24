577e5b7dbe172b718e5553309cd68b84ebedb65d
gigan0815 <amatecki92@gmail.com>
2024-03-24 19:13:08 +0100

    Add project files.

---
+++ STUDY.CONSOLE.MathGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace STUDY.CONSOLE.MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
