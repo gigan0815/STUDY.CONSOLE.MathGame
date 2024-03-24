f047eb63d4fb78e14534a0c3c9669b38031055b2
gigan0815 <amatecki92@gmail.com>
2024-03-24 19:45:27 +0100

    Initial commit. Math game containing the 4 basic operators.

--- STUDY.CONSOLE.MathGame/Program.cs
+++ STUDY.CONSOLE.MathGame/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("MathGame!");
+            Console.WriteLine("Available operators: +, -, *, /");
+            Console.WriteLine("Please note that divisions only result in full numbers!");
+            Console.WriteLine("");
+
+
+            Console.Write("Please enter the first number: ");
+            string userInput1 = Console.ReadLine();
+            int num1 = 0;
+            try
+            {
+                num1 = Int32.Parse(userInput1);
+            }
+            catch
+            {
+                Console.WriteLine("'{0}' cant be parsed from string to int!", userInput1);
+            }
+
+            Console.Write("Please enter the operator (+, -, *, /): ");
+            string userInput2 = Console.ReadLine();
+            if(userInput2 != "+" && userInput2 != "-" && userInput2 != "*"  && userInput2 != "/")
+            {
+                Console.WriteLine("You have not entered a correct operator.");
+            }
+
+            Console.Write("Please enter the second number: ");
+            string userInput3 = Console.ReadLine();
+            int num2 = 0;
+            try
+            {
+                num2 = Int32.Parse(userInput3);
+            }
+            catch
+            {
+                Console.WriteLine("'{0}' cant be parsed from string to int!", userInput3);
+            }
+
+            int result = 0;
+            if(userInput2 == "+")
+            {
+                result = num1 + num2;
+            }
+            else if (userInput2 == "-")
+            {
+                result = num1 - num2;
+            }
+            else if (userInput2 == "*")
+            {
+                result = num1 * num2;
+            }
+            else if (userInput2 == "/")
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine("The result is: {0}", result);
         }
     }
 }
